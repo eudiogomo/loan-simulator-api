ea2bd091cfb5cffa543af291618afa024d7fd368
Diogo Marcos <contato@diogomarcos.com>
2020-06-17 19:15:51 -0300

    Adding the final Institution Fee controller

--- app/Http/Controllers/InstitutionFeeController.php
+++ app/Http/Controllers/InstitutionFeeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\InstitutionFee;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\InstitutionFeeRequest;
 
 /**
  * InstitutionFeeController
@@ -34,6 +36,67 @@ class InstitutionFeeController extends Controller
     }
     
     /**
+     * Loan Simulator
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(InstitutionFeeRequest $request)
+    {
+        $array = array();
+        $result = array();
+        $storage = $this->institutionfee->getAll();
+
+        try {
+            $loan_amount = request('valor_emprestimo');
+            $institutions = request('instituicoes');
+            $agreement = request('convenios');
+            $portion = request('parcela');
+
+            foreach ($storage as &$item) {
+                if ($this->checkData($institutions, $item['instituicao'])) {
+                    if ($this->checkData($agreement, $item['convenio'])) {
+                        if ($item['parcelas'] === (int)$portion || $portion === null) {
+                            array_push($array, $item);
+                        }
+                    }
+                }
+            }
+
+            if (count($array) === 0) {
+                return response()->json([
+                    'message' => 'It was not possible to perform the simulation with the reported data!',
+                    'status' => 403,
+                    'error' => 'Error'
+                ], 403);
+            }
+
+            foreach ($array as &$item) {
+                if (!isset($result[$item['instituicao']])) {
+                    $result[$item['instituicao']] = array();
+                }
+
+                $new_item = array(
+                    'parcelas' => $item['parcelas'],
+                    'valor_parcela' => round($loan_amount * $item['coeficiente'], 2),
+                    'taxa' => $item['taxaJuros'],
+                    'convenio' => $item['convenio']
+                );
+
+                array_push($result[$item['instituicao']], $new_item);
+            }
+
+            return response()->json([$result, "status" => 200], 200);
+        } catch (\Exception  $e) {
+            return response()->json([
+                "message" => "The information sent is invalid. Try again!", 
+                "status" => 403, 
+                "error" => $e->getMessage()
+            ], 403);
+        }
+    }
+    
+    /**
      * getInstitutionFee
      *
      * @return void
@@ -49,5 +112,27 @@ class InstitutionFeeController extends Controller
                 "error" => $e->getMessage()
             ], 404);
         }
+    }
+    
+    /**
+     * checkData
+     *
+     * @param  array $array
+     * @param  string $value
+     * @return boolean
+     */
+    private function checkData($array, $value)
+    {
+        if ($array === null) {
+            return true;
+        }
+
+        foreach ($array as &$item) {
+            if ($value === $item) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

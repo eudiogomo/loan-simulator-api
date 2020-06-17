43e5bb800eaa2d3d9c292a3d26b59b38810c52f0
Diogo Marcos <contato@diogomarcos.com>
2020-06-17 17:20:26 -0300

    adding controller for Agreement

---
+++ app/Http/Controllers/AgreementController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Agreement;
+
+/**
+ * AgreementController
+ */
+class AgreementController extends Controller
+{
+    private $agreement;
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $agreement
+     * @return void
+     */
+    public function __construct(Agreement $agreement) 
+    {
+        $this->agreement = $agreement;
+    }
+    
+    /**
+     * Display a listing of the Agreements
+     *
+     * @return void
+     */
+    public function index()
+    {
+        try {
+            return response()->json([$this->agreement->getAll(), "status" => 200], 200);
+        } catch (\Exception  $e) {
+            return response()->json([
+                "message" => "It was not possible to read the agreements!", 
+                "status" => 404, 
+                "error" => $e->getMessage()
+            ], 404);
+        }
+    }
+}
